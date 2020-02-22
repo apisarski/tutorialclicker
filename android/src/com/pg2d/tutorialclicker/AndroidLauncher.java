d1c7ff5c7e9f31682860aa43975f67369a687cab
Andrzej <a.pisarski@uwb.edu.pl>
2020-02-22 14:26:19 +0100

    init

---
+++ android/src/com/pg2d/tutorialclicker/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pg2d.tutorialclicker;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pg2d.tutorialclicker.TutorialClicker;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TutorialClicker(), config);
+	}
+}
---
+++ desktop/src/com/pg2d/tutorialclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pg2d.tutorialclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pg2d.tutorialclicker.TutorialClicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClicker(), config);
+	}
+}
